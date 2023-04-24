0af8a8ba40e6637ac29c3a7131d90282368ebe6b
waleed_alzamil <waleedalzamil80@gmail.com>
2023-04-24 06:14:20 +0200

    final version by Waleed

---
+++ Milestone_1/Loss_Functions/Ms1_EggHolder_FunctionLoss.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+def EggHolder(X):
+    """
+    Computes the value of the EggHolder function at a given point x using TensorFlow.
+
+    Parameters:
+        X (tf.Tensor): A TensorFlow tensor representing the decision variable.
+
+    Returns:
+        tf.Tensor: The value of the EggHolder function at X.
+    """
+    x1, x2 = X[0], X[1]
+
+    term1 = tf.multiply(-1 * (x2 + 47), tf.sin(tf.sqrt(tf.abs(x2 + tf.multiply(x1, 0.5) + 47))))
+    term2 = tf.multiply(x1, tf.sin(tf.sqrt(tf.abs(x1 - (x2 + 47)))))
+
+    return (term1 - term2)
---
+++ Milestone_1/Loss_Functions/Ms1_Trid_FunctionLoss.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+
+def Trid(X):
+    """
+    Computes the value of the Trid function at a given point x using TensorFlow.
+
+    Parameters:
+        X (tf.Tensor): A TensorFlow tensor representing the decision variables.
+
+    Returns:
+        tf.Tensor: The value of the Michalewicz function at X.
+    """
+    sum0 = 0
+    for i in range(X.shape[0]):
+        if i == (X.shape[0] - 1):
+            break
+        sum0 += X[i+1]*X[i]
+    return (tf.reduce_sum((X - 1)**2) - sum0)
+
+
+def TridOptmValue(X):
+    """
+    Computes the value of the Trid function using the diven formula to test the optimizers.
+
+    Parameters:
+        X (tf.Tensor): A TensorFlow tensor representing the decision variables.
+
+    Returns:
+        float: The value of the optimum X that the optimzer must reach.
+    """
+    d= X.shape[0]
+    return (-d * (d + 4) * (d - 1) / 6)
