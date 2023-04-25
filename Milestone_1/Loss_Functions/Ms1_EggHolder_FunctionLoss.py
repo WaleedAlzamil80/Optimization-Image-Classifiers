7681c35e9b144407586ba8df182d6a21a50196cd
MohammadDallash <DeDallash@gmail.com>
2023-04-25 04:31:04 +0200

    the final version of EggHolder and StyblinskiTang

--- Milestone_1/Loss_Functions/Ms1_EggHolder_FunctionLoss.py
+++ Milestone_1/Loss_Functions/Ms1_EggHolder_FunctionLoss.py
@@ -6,10 +6,10 @@ def EggHolder(X):
     Computes the value of the EggHolder function at a given point x using TensorFlow.
 
     Parameters:
-        X (tf.Tensor): A TensorFlow tensor representing the decision variable.
+        X (tf.Tensor of shape (2, n)): A TensorFlow tensor representing the decision variable.
 
     Returns:
-        tf.Tensor: The value of the EggHolder function at X.
+        tf.Tensor of shape (n, ): The value of the EggHolder function at X.
     """
     X = tf.cast(X, dtype=tf.float32)
     x1, x2 = X[0], X[1]
--- Milestone_1/Loss_Functions/Ms1_StyblinskiTang_FunctionLoss.py
+++ Milestone_1/Loss_Functions/Ms1_StyblinskiTang_FunctionLoss.py
@@ -6,15 +6,16 @@ def StyblinskiTang(X):
     Computes the value of the StyblinskiTang function at a given point x using TensorFlow.
 
     Parameters:
-        X (tf.Tensor): A TensorFlow tensor representing the decision variable.
+        X (tf.Tensor of shape (d, n)): A TensorFlow tensor representing the decision variable.
 
     Returns:
-        tf.Tensor: The value of the StyblinskiTang function at X.
+        tf.Tensor of shape (n, ): The value of the EggHolder function at X.
     """
 
+    X = tf.cast(X, dtype=tf.float32)
     f =  tf.subtract(tf.pow(X, 4), tf.multiply(16, tf.pow(X, 2)))
     s = tf.multiply(5, X)
 
     sum = tf.multiply(tf.add(f, s), 0.5)
 
-    return tf.reduce_sum(sum)
+    return tf.reduce_sum(sum, 0)
