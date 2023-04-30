7875b380496fd6bbfd7404eb4801554429f310da
MohammadDallash <DeDallash@gmail.com>
2023-04-30 06:54:04 +0300

    Update Ms1_Michalewicz_FunctionLoss.py

--- Milestone_1/Loss_Functions/Ms1_Michalewicz_FunctionLoss.py
+++ Milestone_1/Loss_Functions/Ms1_Michalewicz_FunctionLoss.py
@@ -13,9 +13,17 @@ def Michalewicz(X, m = 10):
         tf.Tensor: The value of the Michalewicz function at X.
     """
 
-    i = np.array(range(X.shape[0]), dtype = np.float32).reshape(X.shape[0],1) + 1
-    i = tf.Variable(i)
+    if (tf.rank(X) == 3):
+        i = np.arange(  1,X.shape[0]+1)  .reshape(X.shape[0], 1, 1) + np.zeros(   (1, X.shape[1], X.shape[2]),  dtype = np.float32)
+    else:
+        i = np.array(range(X.shape[0]), dtype = np.float32).reshape(X.shape[0],1) + 1
+    
 
+    i = tf.Variable(i, dtype=tf.float32)
+    X = tf.cast(X, dtype=tf.float32)
+
+
+ 
     M = tf.sin(tf.math.multiply(tf.math.multiply(X, X), i) / np.pi)**(2 * m)
     S = tf.math.multiply(tf.sin(X), M)
 
