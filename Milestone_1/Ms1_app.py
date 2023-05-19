0c4d0bcf88eb9e4df9fe89e1211f6421a0f172ae
abdullah-younnis <abdullahyounnis@gmail.com>
2023-05-19 22:52:37 +0300

    building monitoring system with 1 test_function/1 Opt_algo

--- Milestone_1/Ms1_app.py
+++ Milestone_1/Ms1_app.py
@@ -1,94 +1,81 @@
 import streamlit as st
 import numpy as np
 import inspect
-# importing the optimization algorithms
-from Optimization_Algorithms.Ms1_AdadeltaOpt import *
-from Optimization_Algorithms.Ms1_AdagradOpt import *
-from Optimization_Algorithms.Ms1_AdamOpt import *
+import tensorflow as tf
+import matplotlib.pyplot as plt
+# # importing the optimization algorithms
+# from Optimization_Algorithms.Ms1_AdadeltaOpt import *
+# from Optimization_Algorithms.Ms1_AdagradOpt import *
+# from Optimization_Algorithms.Ms1_AdamOpt import *
 from Optimization_Algorithms.Ms1_GDMOpt import *
-from Optimization_Algorithms.Ms1_SGDOpt import *
+# from Optimization_Algorithms.Ms1_SGDOpt import *
 # importing test function
-from Loss_Functions.Ms1_3HumpCamel_FunctionLoss import *
-from Loss_Functions.Ms1_EggHolder_FunctionLoss import *
+# from Loss_Functions.Ms1_3HumpCamel_FunctionLoss import *
+# from Loss_Functions.Ms1_EggHolder_FunctionLoss import *
 from Loss_Functions.Ms1_MATYAS_FunctionLoss import *
-from Loss_Functions.Ms1_Michalewicz_FunctionLoss import *
-from Loss_Functions.Ms1_StyblinskiTang_FunctionLoss import *
-from Loss_Functions.Ms1_Trid_FunctionLoss import *
+# from Loss_Functions.Ms1_Michalewicz_FunctionLoss import *
+# from Loss_Functions.Ms1_StyblinskiTang_FunctionLoss import *
+# from Loss_Functions.Ms1_Trid_FunctionLoss import *
 
 
-# Define the optimization algorithms
-optimization_algorithms = {
-    'Adadelta': Adadelta,
-    'Adagrad': Adagrad,
-    'Adam': Adam,
-    'GDM' : GDM,
-    'SGD' : SGD
-}
+# Define the optimization algorithm
 
 # Define the test functions
-test_functions = {
-    'ThreeHumpCamel': ThreeHumpCamel,
-    'EggHolder': EggHolder,
-    'MATYAS': MATYAS,
-    'Michalewicz': Michalewicz,
-    'StyblinskiTang': StyblinskiTang,
-    'Trid': Trid ,
-}
 
-X_init = tf.Variable([[-130.0], [-130.0]])
-V_init = tf.Variable([[0.0], [0.0]])
-S_init = tf.Variable([[0.0],[ 0.0]])
-loss_val = [test_function(X_init)]
-X_val = [X_init.np()]
+def plot_optimization(X_val, Y_val):
+    fig, ax = plt.subplots()
+    X1, X2 = np.meshgrid(np.linspace(-10, 10, 100), np.linspace(-10, 10, 100))
+    Z = MATYAS([X1, X2])
+    ax.contour(X1, X2, Z, levels=np.logspace(-0.5, 3.5, 20))
+    ax.plot(*tf.transpose(X_val), Y_val, '-o', color='r')
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-10, 10])
+    st.pyplot(fig)
 
-# Define the Streamlit app
-def app():
-    st.title('Comparing Optimization Algorithms on Test Functions')
+def main():
+    st.title("Matyas Function Optimization with GDM Algorithm CI Project")
 
-    # Add a dropdown to select the optimization algorithm
-    optimization_algorithm = st.selectbox(
-        'Select Optimization Algorithm',
-        list(optimization_algorithms.keys()))
 
-    # Add a dropdown to select the test function
-    test_function = st.selectbox(
-        'Select Test Function',
-        list(test_functions.keys()))
+    st.markdown(
+        """
+        <style>
+        body {
+                background-color: #f5f5f5;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    # Set up initial values
+    X_init = tf.Variable(tf.ones((2,)))
+    V_init = tf.Variable(tf.zeros((2,)))
+    loss_val = []
+    X_val = []
 
-    num_iterations = st.slider('Number of Iterations', 10, 100)
-    
+    # Set up optimization parameters
+    eta = st.slider("LearningRate", 0.001, 1.0, 0.01, 0.001)
+    beta = st.slider("Momentum Hyperparameter", 0.0, 1.0, 0.9, 0.01)
+    bias_correction = st.checkbox("Bias Correction", value=False)
+    line_search = st.checkbox("Line Search", value=False)
 
-    for i in range(num_iterations):
-        if optimization_algorithm == "SGD":
-            result = SGD(test_function, X_init, loss_val, X_val)
-        elif optimization_algorithm == "GDM":
-            result = GDM(test_function,  X_init, V_init, loss_val, X_val)
-        elif optimization_algorithm == "Adagrad":
-            result = Adagrad(test_function, X_init, S_init, loss_val, X_val)
-        elif optimization_algorithm == "Adadelta":
-            result = Adadelta(test_function, X_init, S_init, loss_val, X_val)
-        elif optimization_algorithm == "Adam":
-            result = Adam(test_function, X_init, V_init, S_init, loss_val, X_val)    
+    # Set up learning rate decay parameters
+    decay_type = st.selectbox("Learning Rate Decay Type", ["None", "Time-Based", "Step-Based", "Exponential", "Performance"])
+    if decay_type != "None":
+        decay_rate = st.slider("Decay Rate", 0.0, 1.0, 0.9, 0.01)
+    else:
+        decay_rate = None
 
-    for i in range(num_iterations):
-        if test_function == "ThreeHumpCamel":
-            result = ThreeHumpCamel()
-        elif test_function == "EggHolder":
-            result = EggHolder()
-        elif test_function == "MATYAS":
-            result = MATYAS()
-        elif test_function == "Michalewicz":
-            result = Michalewicz()
-        elif test_function == "StyblinskiTang":
-            result = StyblinskiTang()
+    # Run optimization
+    for i in range(500):
+        GDM(MATYAS, X_init, V_init, loss_val, X_val, eta=eta, beta=beta, bias_correction=bias_correction,
+            line_search=line_search, t=i, decay_type=decay_type, decay_rate=decay_rate)
 
-        # Run the selected optimization algorithm on the selected test function with the user input parameters
+    # Plot results
+    st.write("Final Result:", X_val[-1])
+    st.write("Final Loss:", loss_val[-1])
+    plot_optimization(np.array(X_val), loss_val)
 
-        # Visualize the results using a scatter plot
-        x = np.arange(num_iterations)
-        y = result[[test_function,optimization_algorithm, 0.01,False,False ,X_val,loss_val]]
-        st.line_chart({'x': x, 'y': y})
-
-# Run the Streamlit app
 if __name__ == '__main__':
-    app()
+    main()
