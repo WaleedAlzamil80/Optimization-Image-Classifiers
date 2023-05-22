9c6b65936d410dfa1157df37bc5ca9b2a9ac90f7
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-05-22 15:38:48 +0300

    Classical Archi

---
+++ Milestone_2/Models/E_Archi.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+
+def B(input_shape, num_classes):
+  model = tf.keras.Sequential()
+
+  model.add(tf.keras.layers.Input(input_shape))
+  model.add(tf.keras.layers.Rescaling(1./255))
+  
+  model.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.Conv2D(filters = 64, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.MaxPooling2D((2,2)))
+  model.add(tf.keras.layers.Dropout(0.1))
+
+  model.add(tf.keras.layers.Conv2D(filters = 128, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.Conv2D(filters = 128, kernel_size = (3,3), activation = "relu", padding = "same")) 
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.MaxPooling2D((2,2)))
+  model.add(tf.keras.layers.Dropout(0.1))
+
+  model.add(tf.keras.layers.Conv2D(filters = 256, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 256, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 256, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 256, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.MaxPooling2D((2,2)))
+  model.add(tf.keras.layers.Dropout(0.1))
+
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.MaxPooling2D((2,2)))
+  model.add(tf.keras.layers.Dropout(0.1))
+
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3), activation = "relu", padding = "same"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.MaxPooling2D((2,2)))
+  model.add(tf.keras.layers.Dropout(0.1))
+
+  model.add(tf.keras.layers.Flatten())
+
+  model.add(tf.keras.layers.Dense(units = 512, activation = "relu"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Dropout(0.2))
+  model.add(tf.keras.layers.Dense(units = 128, activation = "relu"))
+  model.add(tf.keras.layers.BatchNormalization())
+  model.add(tf.keras.layers.Dropout(0.2))
+
+  model.add(tf.keras.layers.Dense(units = 100, activation = "softmax"))
+
+  model.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics=['accuracy'])
+
+  return model
--- Milestone_2/README.md
+++ Milestone_2/README.md
@@ -6,7 +6,7 @@
 - epocks: 50
 - callback: earlystopping after 3 epochs if the val_loss didn't decrease
 - train, test, validation:  42k, 10k, 8k
-- Batch size: Not Specified (we need to use the same batch for all architectures)
+- batch_size: 256
 - No preprocessing (can be modified and do some of them but before must be satetd here and on the group)
 - Use the following function to save the progress in case the colabe is disconnected
 ```
